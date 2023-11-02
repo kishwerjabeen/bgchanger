5cb9af9ea2ee92fb5595acf51649d630fddb09d1
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-11-02 12:01:30 +0500

    name add

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   return (
     <>
       <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+      <h1 className="text-white flex justify-center pt-20 text-5xl">Kishwar J.</h1>
         <div className="fixed flex flex-wrap
         justify-center bottom-12 inset-x-0 px-2">
+
+
+          
 
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
 
